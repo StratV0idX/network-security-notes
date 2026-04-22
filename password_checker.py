387e29ad866d05d35393427cab6b1a38d4ec5ac7
Roshan Jeffrin R <roshanjeffrinr@gmail.com>
2026-04-22 17:45:48 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -2,26 +2,6 @@
 # Original Author: Shannel Segwabe
 # Contributor: Roshan Jeffrin R
 # Changes: Added scoring system, improved validation, user feedback
-"""
-import re
-
-def check_password(password):
-    if len(password) < 8:
-        return "Weak: Too short"
-    if not re.search(r"[A-Z]", password):
-        return "Weak: Add uppercase letter"
-    if not re.search(r"[a-z]", password):
-        return "Weak: Add lowercase letter"
-    if not re.search(r"[0-9]", password):
-        return "Weak: Add number"
-    if not re.search(r"[!@#$%^&*()_+]", password):
-        return "Weak: Add special character"
-    return "Strong Password"
-
-if __name__ == "__main__":
-    password = input("Enter password to check: ")
-    print(check_password(password))
-"""
 import re
 import os
 from tkinter import *
